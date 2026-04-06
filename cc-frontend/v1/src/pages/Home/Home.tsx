b8a473df5e647aed9938cc0298fe822a1a62a60d
aidandegooyer <aidandegooyer@gmail.com>
2026-04-06 14:53:45 -0400

    header update

--- cc-frontend/v1/src/pages/Home/Home.tsx
+++ cc-frontend/v1/src/pages/Home/Home.tsx
@@ -13,30 +13,10 @@ function Home() {
     <div className="app-container mx-4 mt-2 flex justify-center">
       <div className="home w-full max-w-[1200px]">
         <NavLink to="/events">
-          <div className="grid grid-cols-5 gap-4">
-            <div className="col-span-5 my-4 flex h-32 w-full flex-col items-center justify-center rounded-xl border-2 md:col-span-3">
+          <div className="grid grid-cols-1 gap-4">
+            <div className="my-4 flex h-32 w-full flex-col items-center justify-center rounded-xl border-2 md:col-span-3">
               <img src={c4_logo} alt="C4 Logo" className="-m-3 h-24" />
-              <h1 className="flex items-center text-3xl">
-                Returns for February!
-              </h1>
-            </div>
-            <div className="col-span-1 my-4 hidden h-32 w-full flex-col items-center justify-center rounded-xl border-2 md:flex">
-            <h1 className="flex items-center text-xl">
-                Previous Champions:
-              </h1>
-              <img
-                src={
-                  "https://legacyplatformapiprod.blob.core.windows.net/images/teams/4f0e0828-41d2-480e-869b-d274af8c13e6.jpg?v=638437415076048339"
-                }
-                alt="C4 Logo"
-                className="h-16 w-16 rounded-md"
-              />
-              <h1 className="flex items-center text-xl">RIT Orange</h1>
-            </div>
-            <div className="col-span-1 my-4 hidden h-32 w-full flex-col items-center justify-center rounded-xl border-2 md:flex">
-              <h1 className="flex items-center text-center text-2xl">
-                Welcome Back for Spring 2026!
-              </h1>
+              <h1 className="flex items-center text-3xl">Returns for April!</h1>
             </div>
           </div>
         </NavLink>
